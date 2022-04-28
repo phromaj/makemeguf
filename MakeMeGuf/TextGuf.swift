c160217f96cf5ab8b81c8cb75bf25613aed7ec76
progresso85 <martin.heuripeau@gmail.com>
2022-04-28 15:15:12 +0200

    add class TextGuf
    TextGuf

---
+++ MakeMeGuf/TextGuf.swift
@@ -0,0 +1,11 @@
+//
+//  TextGuf.swift
+//  MakeMeGuf
+//
+//  Created by Martin HEAURIPEAU on 28/04/2022.
+//  Copyright © 2022 guf. All rights reserved.
+//
+
+import Foundation
+
+let searchGuf = ["Quand y'a plus de ... à la cantine","Fermez-la et prenez mon argent !", "Pourriez-vous me montrer votre ... ?!","9 + 10 = 21", "Deus Vult","Alt + F4","Première fois ?","Allez au Choppa!", "rush B","Sco Pa Tu Manaa","tout le monde est gay pour Bridget","La paix n'a jamais été une option","Mon argent ne s'agite pas, il se plie.","Tu vas être incroyable", "Les gars, j'ai de mauvaises nouvelles", "D'abord, c'est un truc du samedi soir", "c'est pas toi qui décide", "Mais t'es pas net", "Aucun de ces mots n'est dans la Bible" , "Merci, je déteste ça", "Tu es un sorcier, Harry !", "Je sollicite à nouveau votre soutien financier", "Je n'ai jamais demandé ça", "Je modifie l'accord", "Nous sommes des destructeurs de bites", "Attendez, c'est illégal", "Ce post m'a donné le cancer", "Excusez-moi, c'est quoi ce bordel ?", "La poudre qui vous fait dire oui","Regardez-moi, je suis le capitaine maintenant"]
