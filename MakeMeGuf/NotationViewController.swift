7ae0249babe1342dbb1894872a7d775f90ab96a8
LenaPancher <lenapancher@gmail.com>
2022-04-28 10:04:17 +0200

    Update storyboard

---
+++ MakeMeGuf/NotationViewController.swift
@@ -0,0 +1,30 @@
+//
+//  NotationViewController.swift
+//  MakeMeGuf
+//
+//  Created by Léna PANCHER on 28/04/2022.
+//  Copyright © 2022 guf. All rights reserved.
+//
+
+import UIKit
+
+class NotationViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
